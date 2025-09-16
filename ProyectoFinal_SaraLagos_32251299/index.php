072569ecbc02cafdf5011b993095ad0b9a971ff2
Liss06 <michellegarciasanchez06@gmail.com>
2025-09-15 22:00:41 -0600

    Que te importa

--- ProyectoFinal_SaraLagos_32251299/index.php
+++ ProyectoFinal_SaraLagos_32251299/index.php
@@ -57,7 +57,7 @@
     <?php foreach($tablas as $tabla) { ?>
       <div class="productos">
         <a href="tabla.php?tabla=<?php echo $tabla; ?>">
-          <img class="producto-imagen" src="img/<?php echo $tabla; ?>.jpg" alt="Imagen <?php echo $tabla; ?>">
+          <img class="producto-imagen" src="img/<?php echo $tabla; ?>.png" alt="Imagen <?php echo $tabla; ?>">
           <div class="producto-informacion">
             <p class="producto-nombre"><?php echo ucfirst($tabla); ?></p>
           </div>
